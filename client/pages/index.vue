c91131851d1d87db79281dc3ce01294ab4b3aef5
nikita.kukhar <nikita.kukhar@best2pay.net>
2024-12-02 18:28:46 +0300

    feat: инициализация фронт-приложения

---
+++ client/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: 'default',
+})
+</script>
+<template>
+    <div class="flex">
+        <h1 class="text-3xl text-gray-500">Hello Hackathon</h1>
+    </div>
+</template>
+
+<style scoped></style>
