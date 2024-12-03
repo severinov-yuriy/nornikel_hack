a4eaa05b168d25e76a5ef6c5abe546e7fc956546
BeingAsNikita <42153452+BeingAsNikita@users.noreply.github.com>
2024-12-03 16:04:52 +0300

    feat: добавил tailwind, немного верстки (#2)
    Co-authored-by: nikita.kukhar <nikita.kukhar@best2pay.net>

---
+++ client/src/app/layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { VHeader } from '@/widgets/header'
+</script>
+
+<template>
+    <div class="flex min-h-[100vh] flex-col bg-slate-100 dark:bg-slate-900">
+        <VHeader />
+        <div class="flex flex-col px-4 py-6">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ client/src/features/theme-toggle/ui/ThemeToggle.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const isDark = computed({
+    get() {
+        return colorMode.value === 'dark'
+    },
+    set() {
+        colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+    },
+})
+</script>
+<template>
+    <UToggle
+        size="lg"
+        v-model="isDark"
+        on-icon="i-heroicons-sun"
+        off-icon="i-heroicons-moon"
+    />
+</template>
+
+<style scoped></style>
