376ca16d15062da3c838abfc75c8fbeb81b5576f
Yuriy Severinov <yura.severinov@yandex.ru>
2024-12-06 16:40:00 +0300

    Добавил загрузку файлов, структуру запроса

--- server/app/routes/query.py
+++ server/app/routes/query.py
@@ -3,8 +3,16 @@ from pydantic import BaseModel
 
 router = APIRouter()
 
+class QueryRequest(BaseModel):
+    query: str
+    api_type: str = None
+    api_key: str = None
+    api_url: str = None  # Для кастомных моделей
+    model_name: str = "meta-llama/llama-3.1-70b-instruct:free"
+    top_k: int = 1
+
 @router.post("/query/")
-async def handle_query(request):
+async def handle_query(request: QueryRequest):
     """
     Эндпоинт для обработки запросов пользователя с использованием RAG.
     """
--- server/app/routes/upload.py
+++ server/app/routes/upload.py
@@ -1,11 +1,26 @@
-from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+import os
+from fastapi import APIRouter, UploadFile, HTTPException
 
 router = APIRouter()
 
+# Путь для сохранения временных файлов
+TEMP_FOLDER = "data/uploads"
+DB_PATH = "data/chunks.db"
+VECTOR_STORE_PATH = "data/vector_store.index"
+VECTOR_DIM = 384
+
+
 @router.post("/upload/")
-async def handle_query(request):
+async def upload_archive(file: UploadFile):
     """
-    Эндпоинт для обработки запросов пользователя с использованием RAG.
+    Эндпоинт для загрузки архива документов.
     """
-    return f'{request=} received'
+    # Создаем временную папку, если она не существует
+    os.makedirs(TEMP_FOLDER, exist_ok=True)
+
+    # Сохраняем загруженный файл
+    archive_path = os.path.join(TEMP_FOLDER, file.filename)
+    with open(archive_path, "wb") as f:
+        f.write(await file.read())
+
+    return {"message": "Archive processed successfully"}
