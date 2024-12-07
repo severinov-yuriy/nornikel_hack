c26fa570274af402d95b104fa38ad49b949bdf3b
Yuriy Severinov <yura.severinov@yandex.ru>
2024-12-08 02:59:45 +0300

    Поправил удаление файла на бэке

--- server/app/routes/files.py
+++ server/app/routes/files.py
@@ -13,18 +13,17 @@ def get_files(id: Optional[int] = None):
     """
     Эндпоинт для скачивания файла с бэка.
     """
-    print(id)
     if id:
         filename, ext, content_type = get_filename_by_id(id, Config.DB_PATH)
         file_path = os.path.join(Config.FILES_FOLDER, f"{filename}{ext}")
         return FileResponse(path=file_path, filename=f"{filename}{ext}", media_type=content_type)
     return get_files_list(Config.DB_PATH)
 
-@router.delete("/files/")
-def delete_file(id: str):
+@router.delete("/files/{id}")
+def delete_file(id: int):
     """
     Эндпоинт для скачивания файла с бэка.
     """
-    filename, ext = get_filename_by_id(id, Config.DB_PATH)
+    filename, ext, _ = get_filename_by_id(id, Config.DB_PATH)
     os.remove(f'{Config.FILES_FOLDER}/{filename}{ext}')
     return delete_by_id(id, Config.DB_PATH)
